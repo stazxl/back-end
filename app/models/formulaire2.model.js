b94690b57a7667d0b9ae812a171776589fa67c71
Jiqso <keryan.cazaban@gmail.com>
2023-06-30 16:54:42 +0200

    fix(model) : Fixing formulaire error

--- app/models/formulaire2.model.js
+++ app/models/formulaire2.model.js
@@ -1,68 +1,68 @@
-const { dark } = require('@material-ui/core/styles/createPalette');
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-  const Formulaire2 = sequelize.define('formulaire2', {
-    idFormulaire:{
-      type:DataTypes.STRING,
-      unique:true,
-      allowNull:false
+  const Formulaire2 = sequelize.define(
+    "formulaire2",
+    {
+      idFormulaire: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      prenom: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      nom: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      numeroTelephone: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      intitulePoste: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      nomEntreprise: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      adresse: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      codePostal: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      ville: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      urlSiteWeb: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      nombreAbonnes: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      nouveauxInscrisMois: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      active: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    prenom: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    nom: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    numeroTelephone: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    intitulePoste: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    nomEntreprise: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    adresse:{
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    codePostal: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    ville: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    urlSiteWeb: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    nombreAbonnes: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    nouveauxInscrisMois: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    active: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
-  },
-  {sequelize,
-    freezeTableName: true
-  });
+    { sequelize, freezeTableName: true }
+  );
   return Formulaire2;
 };
